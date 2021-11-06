b8eb2d44bdf2cbd578f652e7249a0b6c922ba353
Jens Albrecht <albrecht_jens@icloud.com>
2021-11-06 12:54:50 +0100

    axios integration and simple example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
-  <h1>Hallo on Home</h1>
+  <div>
+    <h1>Hallo on Home</h1>
+    <h2 v-if="this.bitdata!==null">BitCoin Preis: {{ this.bitData.data.bpi.EUR.rate }} {{ this.bitData.data.bpi.EUR.code }}</h2>
+  </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
+  import axios from 'axios'
 
-  export default Vue.extend({
+  export default Vue.extend({ 
     name: 'Home',
+    data: () => ({
+      bitData: null
+    }),
+
+    async mounted() {
+        axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then((response) => {
+          this.bitData = response
+        })
+    }
   })
 </script>
