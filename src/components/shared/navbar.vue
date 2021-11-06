f9b6070ab26b7a764ef1605003d5af7993a18762
Jens Albrecht <albrecht_jens@icloud.com>
2021-11-06 12:27:37 +0100

    integrate base layout und nav

---
+++ src/components/shared/navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-bottom-navigation 
+      grow
+      fixed
+      bottom
+      color="teal"
+      >
+      <v-btn to="/">
+        <span>Home</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn to="/about">
+        <span>About</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+</template>
+
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style>
+    .v-item-group.v-bottom-navigation .v-btn.v-size--default {
+        height: inherit;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,11 @@
 <template>
-  <hello-world />
+  <h1>Hallo on Home</h1>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
 
   export default Vue.extend({
     name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
   })
 </script>
