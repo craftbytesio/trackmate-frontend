c9b0225d0cae338b30d3e398a8082c93dc46dd58
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-11-08 15:07:59 +0100

    Add buttons for login and logout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,30 @@
   <div>
     <h1>Hallo on Home</h1>
     <h2 v-if="this.bitdata!==null">BitCoin Preis: {{ this.bitData.data.bpi.EUR.rate }} {{ this.bitData.data.bpi.EUR.code }}</h2>
+    <button v-if="this.$store.getters['auth/loggedIn']" @click="logout">Logout</button>
+    <button v-if="!this.$store.getters['auth/loggedIn']" @click="loginView">Login</button>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
   import axios from 'axios'
+  import AuthService from '@/service/AuthService';
 
   export default Vue.extend({ 
     name: 'Home',
     data: () => ({
       bitData: null
     }),
+    methods: {
+      loginView(){
+        this.$router.push('login');
+      },
+      async logout(){
+        await AuthService.logout();
+        this.$router.push('/login');
+      }
+    },
 
     async mounted() {
         axios
