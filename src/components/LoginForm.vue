b02681075541a846c192887a5d0074fb3b964ebc
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-11-11 10:23:49 +0100

    Update login method

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -28,7 +28,6 @@
 </template>
 
 ><script lang="ts">
-import AuthService from '@/service/AuthService';
 import Vue from 'vue'
 
 export default Vue.extend({
@@ -41,27 +40,15 @@ export default Vue.extend({
         }
     },
     methods: {
-        async login(){
+        login(){
           const payload = {
             email: this.email,
             password: this.password
           };
           this.error = null;
-          try {
-            await AuthService.login(payload);
-            const authUser = await this.$store.dispatch("auth/getAuthUser");
-            if (authUser) {
-              this.$router.push("/home");
-            } else {
-              const error = Error(
-                  "Unable to fetch user after login, check your API settings."
-              );
-              error.name = "Fetch User";
-              throw error;
-            }
-          } catch (error){
-            this.error = error;
-          }
+          this.$store.dispatch('login', payload)
+          .then(() => this.$router.push({name: 'Home'}))
+          .catch(err => this.error = err);
         }
     }
 })
