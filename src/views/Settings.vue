cd6d0f5c63a06cb6145bc31e154541ca1e908606
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-12-05 16:42:03 +0100

    Use LanguageSelect component

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -7,15 +7,7 @@
   >
     <v-card-title>{{ $t('settings') }}</v-card-title>
     <v-card-text>
-      <v-select
-          v-model="language.locale"
-          :items="languages"
-          item-text="name"
-          item-value="locale"
-          :label="$t('selectLanguageLabel')"
-          single-line
-          @change="updateLanguage"
-      ></v-select>
+      <LanguageSelect />
     </v-card-text>
   </v-card>
 </template>
@@ -23,28 +15,15 @@
 <script>
 import Vue from 'vue'
 import {mapGetters} from "vuex"
-import i18n from '../plugins/i18n'
+import LanguageSelect from "../components/shared/LanguageSelect";
 
 export default Vue.extend({
   name: 'Settings',
-  methods: {
-    updateLanguage(locale) {
-      i18n.locale = locale
-      this.$store.dispatch('updateUserLanguage', locale)
-    },
+  components: {
+    LanguageSelect
   },
   computed: {
     ...mapGetters({user: 'getCurrentUser'}),
   },
-  data() {
-    let userLocale = this.$store.getters.getCurrentUser.language
-    return {
-      language: {locale: userLocale},
-      languages: [
-        {name: 'Deutsch', locale: 'de'},
-        {name: 'English', locale: 'en'}
-      ]
-    }
-  }
 });
 </script>
