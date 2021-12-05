3edb313e2391fb6f52316f2b242dd39c7cd0d89f
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-12-05 16:41:34 +0100

    Init register view

---
+++ src/views/Register.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card
+      elevation="2"
+      shaped
+      class="mx-auto mt-5"
+      max-width="400"
+  >
+    <v-card-title>{{ $t('register') }}</v-card-title>
+    <RegisterForm class="p-5 bg-white border rounded shadow" />
+
+  </v-card>
+</template>
+
+<script>
+import Vue from 'vue'
+import RegisterForm from "../components/RegisterForm";
+
+export default Vue.extend({
+  name: "RegisterView",
+  components: {
+    RegisterForm,
+  },
+});
+</script>
